8bca63f56b10e8b903802c3e6bcd5e519926d070
VerteDinde <keeleymhammond@gmail.com>
2021-01-02 20:50:02 -0800

    feat: add vec3 class

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,7 @@
+#include "color.h"
+#include "vec3.h"
+
 #include <iostream>
-using namespace std;
 
 int main()
 {
@@ -8,25 +10,18 @@ int main()
   const int image_height = 256;
 
   // Render
-  cout << "P3\n"
-            << image_width << ' ' << image_height << "\n255\n";
+  std::cout << "P3\n"
+       << image_width << ' ' << image_height << "\n255\n";
 
   for (int j = image_height - 1; j >= 0; --j)
   {
-    cerr << "\rScanlines remaining: " << j << ' ' << flush;
+    std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
     for (int i = 0; i < image_width; ++i)
     {
-      auto r = double(i) / (image_width - 1);
-      auto g = double(j) / (image_height - 1);
-      auto b = 0.25;
-
-      int ir = static_cast<int>(255.999 * r);
-      int ig = static_cast<int>(255.999 * g);
-      int ib = static_cast<int>(255.999 * b);
-
-      cout << ir << ' ' << ig << ' ' << ib << '\n';
+      color pixel_color(double(i) / (image_width - 1), double(j) / image_height - 1, 0.25);
+      write_color(std::cout, pixel_color);
     }
   }
 
-  cerr << "\nDone.\n";
+  std::cerr << "\nDone.\n";
 }
